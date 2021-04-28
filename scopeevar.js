bbdde9f237dbb6cbc1cdae142b4e74407ffd35bd
Nycole <nycole.xavierr@gmail.com>
2021-04-28 07:45:51 -0300

    scope and var

---
+++ scopeevar.js
@@ -0,0 +1,15 @@
+// scope
+
+// escopo determina a visibilidade de alguma variável no JS
+
+// Block statement
+
+// vamos iniciar um bloco
+//{ vamos iniciar um bloco
+    // aqui dentro é um bloco e posso colocar qualquer código
+
+//} aqui fechamos o bloco
+
+// O bloco também criará um novo escopo. Chamamos de `block-scoped`
+
+// var é global e, também local 
