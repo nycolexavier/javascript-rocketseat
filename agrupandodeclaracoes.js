0c2e48ed32b076a685cc8446d9974dc7a7bdd401
Nycole <nycole.xavierr@gmail.com>
2021-04-30 16:12:26 -0300

    agrupando declarações

--- agrupandodeclaracoes.js
+++ agrupandodeclaracoes.js
@@ -1,3 +1,5 @@
+let name = "Mike"
+
 // agrupamento de declarações
 let age, isHuman
 
@@ -5,5 +7,14 @@ let age, isHuman
 age = 20
 isHuman= true
 
+
+//multiplos argumentos na função
 console.log(age, isHuman)
 
+// escrita de texto + variáveis
+
+// concatenação
+console.log('O' + name + )
+
+
+// interpolando valores com templ
