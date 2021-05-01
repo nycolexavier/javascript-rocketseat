3b548dd03a2f63022a54ff5002026f55c11c717d
Nycole <nycole.xavierr@gmail.com>
2021-05-01 10:11:28 -0300

    funções construtoras

---
+++ funções/funcoesconstrutoras.js
@@ -0,0 +1,22 @@
+/*
+    Function() constructor
+
+    *expressão new
+    *criar um novo projeto
+    *this keyword
+*/
+
+let date = new Date ("2020-12-01")
+console.log(date)
+
+
+//function Person(name) {
+    //this.name = name
+    //this.walk = function() {
+        //return this.name + " está andando"
+  //  }
+//}
+//const mayk = new Person('Mayk')
+//const joao = new Person ('João')
+//console.log(mayk.walk())
+//console.log(joao.walk())
