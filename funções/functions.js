db8545110d3e2be69db690a881aa0096b3cedcc6
Nycole <nycole.xavierr@gmail.com>
2021-05-01 07:18:38 -0300

    functions

---
+++ funções/functions.js
@@ -0,0 +1,13 @@
+// criar um aplicativo de frases motivacionais
+
+console.log('Estudar é muito bom')
+console.log('Paciência e persistência')
+console.log('Revisão é a mãe do aprendizado')
+
+console.log('Estudar é um privilégio')
+console.log('Comece hoje')
+console.log('foco e disciplina')
+
+console.log('Não importa se o dia foi bom ou ruim, vá estudar!')
+console.log('Estude! Porque você nunca vai ouvir dizer que se arrependeu de ter estudado')
+console.log('Estudar é crescer em silêncio')
