d475d789b499d2719614d171c5a0fa2ec5f948d5
Nycole <nycole.xavierr@gmail.com>
2021-05-01 09:16:50 -0300

    functions

--- funções/functions.js
+++ funções/functions.js
@@ -1,13 +1,18 @@
 // criar um aplicativo de frases motivacionais
 
-console.log('Estudar é muito bom')
-console.log('Paciência e persistência')
-console.log('Revisão é a mãe do aprendizado')
 
-console.log('Estudar é um privilégio')
-console.log('Comece hoje')
-console.log('foco e disciplina')
+//declaration - declaração da funcção
+// funcion statement
+ function createPhrases() {
+    console.log('Estudar é muito bom')
+    console.log('Paciência e persistência')
+    console.log('Revisão é a mãe do aprendizado')
+}
 
-console.log('Não importa se o dia foi bom ou ruim, vá estudar!')
-console.log('Estude! Porque você nunca vai ouvir dizer que se arrependeu de ter estudado')
-console.log('Estudar é crescer em silêncio')
+
+// executar a função
+// rodar, chamar, invocar
+// execute, run, call, invoke
+
+createPhrases()
+console.log('Fim do programa')
