95a033016eb6949c851100e413d0e3ba5616b266
Nycole <nycole.xavierr@gmail.com>
2021-05-01 09:24:01 -0300

    arguments and paramenters

---
+++ funções/argumentoeparametros.js
@@ -0,0 +1,10 @@
+// function expression
+// function anonymous
+
+
+// parâmetros (parameters)
+const sum = function(number1, number2) {
+    console.log(number1+ number2)
+}
+
+sum(2,3) // arguments - argumentos
