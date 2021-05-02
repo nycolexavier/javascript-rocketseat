808c943c08b771d6a97813b79d2740140231a7fe
Nycole <nycole.xavierr@gmail.com>
2021-05-02 18:34:12 -0300

    Operadores de comparação igual a diferente de

---
+++ expressoeseoperadores/operadoresdecomparacaoigualeadiferentede.js
@@ -0,0 +1,15 @@
+// Operadores de comparação
+
+// Irá comparar valores e retornar um Boolean como resposta á comparação
+
+let one = 1
+let two = 2
+
+// ==   igual a 
+//console.log(two == 1)false
+//console.log(one == "1")true
+
+// !=   diferente de
+//console.log(one != two)true
+//console.log(one != 1)false
+//console.log(one != "1")false 
