e03a354290c57de85316457005858778e77aaebb
Nycole <nycole.xavierr@gmail.com>
2021-05-02 08:34:19 -0300

    parando strings

---
+++ manipulandodados/separandostrings.js
@@ -0,0 +1,8 @@
+// Manipulando Strings e Arrays
+
+// Separe um texto que contem espaços, em um novo array onde cada texto é uma possição do array. Depois disso, transforme o array em um texto e onde eram espaços, coloque _
+
+let phrase = "Eu quero viver o Amor!"
+let myArray = phrase.split(" ")
+let phraseWithUnderscore = myArray.join("_")
+console.log(phraseWithUnderscore.toLowerCase())
