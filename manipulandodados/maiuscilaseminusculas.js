22f147a1fc23450c01197eda915d6a6e697b4ea1
Nycole <nycole.xavierr@gmail.com>
2021-05-02 08:28:15 -0300

    maiúsculas e minúsculas

---
+++ manipulandodados/maiuscilaseminusculas.js
@@ -0,0 +1,6 @@
+// Manipulando Strings e Números
+
+// Transforme letras minúsculas em maiúsculas. Faça o contrário disso também
+
+let word = "programar é muito bacana"
+console.log(word.toLowerCase().toUpperCase())
