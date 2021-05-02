43c4b5b24a7910ed70d0953288d54e85b552d858
Nycole <nycole.xavierr@gmail.com>
2021-05-02 19:01:39 -0300

    Operadores de atribuição

---
+++ expressoeseoperadores/operadoresdeatribuicao.js
@@ -0,0 +1,29 @@
+// Operadores de atribuição (Assignment:atribuição)
+let x
+
+
+// assignment
+x = 1 // x recebe 1
+//console.log(x)
+
+// addition assignmenty
+x += 2 // é a mesma coisa dessa expressão aqui: x = x + 2
+//console.log(x)
+
+// subtraction assignment
+// é a mesma coisa dessa expressão: x = x -1
+x -= 1
+//console.log(x)
+
+// multiplication assignment
+// x *= 2 // mesma coisa: x = x * 2
+// console.log(x)
+
+// division assignment
+// x /= 2 // mesma coisa: x = x / 2
+// console.log(x)
+
+// remainder, exponetiation
+// mesma coisa: x = x % 2
+x %= 2
+console.log(x)
