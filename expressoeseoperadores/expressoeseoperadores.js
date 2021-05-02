d3a3716162c306fdf7d3fd6d68c22c786b0db474
Nycole <nycole.xavierr@gmail.com>
2021-05-02 13:27:47 -0300

    expressões  e operadores

---
+++ expressoeseoperadores/expressoeseoperadores.js
@@ -0,0 +1,14 @@
+/*  Expressões e Operadores
+
+    - Expressions
+    - Operators
+        Binary
+        Unary (--number)
+        Ternary
+
+*/
+// ; no JavaScript é obrigatório
+// let number // isso é um expressão
+let number = 1
+
+console.log(true ? 'alo' : 'nada')
