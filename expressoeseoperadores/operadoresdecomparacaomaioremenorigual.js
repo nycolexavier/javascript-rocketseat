f1dbbb00c844dabfcdab62da8e307c34bec23140
Nycole <nycole.xavierr@gmail.com>
2021-05-02 18:49:44 -0300

    Operadores de comparação maior e menor igual

---
+++ expressoeseoperadores/operadoresdecomparacaomaioremenorigual.js
@@ -0,0 +1,17 @@
+let one = 1
+let two = 2
+
+// >        Maior que
+//console.log(one > two) //false
+
+// >=       Maior igual a
+// console.log(one >= 1) //true
+// console.log(two >= 1) //true
+
+// <        Menor que
+// console.log(one < two) //true
+
+// <=       Menor igual a
+//console.log(one <= two) //true
+//console.log(one <= 1) //true 
+//console.log(one <= 0) //false
