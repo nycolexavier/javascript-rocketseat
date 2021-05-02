801134fbc6384b0e8e90912d9f344b7ec6c5ec75
Nycole <nycole.xavierr@gmail.com>
2021-05-02 08:17:00 -0300

    contando caracteres e dígitos

---
+++ manipulandodados/contandocaracteresedigitos.js
@@ -0,0 +1,8 @@
+// Manipulando strings e Números 
+
+// Contar quantos caracteres tem uma palavra e quantos dígitos tem um número
+
+let word = "Paralelepido"
+console.log(word.length)
+let number = 1234
+console.log(String(number).length)
