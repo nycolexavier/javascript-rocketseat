bc0824f9b05a5064ddd3a8296849834af93984ed
Nycole <nycole.xavierr@gmail.com>
2021-05-02 08:24:02 -0300

    Casas decimais

---
+++ manipulandodados/casasdecimais.js
@@ -0,0 +1,6 @@
+// Manipulando Strings e Números 
+
+// Transformar um número quebrado com 2 casas decimais e trocar ponto por vírgula
+
+let number = 342.954958934
+console.log(number.toFixed(2).replace(".",","))
