83f2fae24101860362378ddf93a5647b81c0d532
Nycole <nycole.xavierr@gmail.com>
2021-05-02 18:23:01 -0300

    Operadores aritiméticos

---
+++ expressoeseoperadores/operadoresaritmeticos.js
@@ -0,0 +1,33 @@
+// Operadores Aritiméticos
+
+// multiplicação *
+//console.log(3.2 * 5.5)
+
+// divisão /
+//console.log(12/2)
+
+// soma +
+//console.log(34 + 67)
+
+// subtração - 
+//console.log( 34 - 23)
+
+// resto da divisão %
+//let remainder
+//remainder = 11 % 3
+//console.log(remainder)
+
+
+// incremento ++ (significa adicionar mais um número)
+//let increment = 0
+//increment++
+//console.log(increment)
+
+
+// decremento -- 
+//let decrement = 0
+//console.log(--decrement)
+
+
+// exponencial **
+//console.log(3 ** 3)
