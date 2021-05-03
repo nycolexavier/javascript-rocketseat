d6da86f009ae1cbf174b9010a97f89335cb4a98e
Nycole <nycole.xavierr@gmail.com>
2021-05-03 12:24:35 -0300

    Switch

---
+++ expressoeseoperadores/switch.js
@@ -0,0 +1,27 @@
+// switch
+
+function calculate(number1, operator, number2) {
+    let result = 0
+
+    switch(operator) {
+        case '+':
+            result = number1 / number2
+            break
+        case '-':
+            result = number1 - number2
+            break
+        case '*':
+            result = number1 * number2
+            break
+        case '/':
+            result: number1 / number2
+            break
+        default:
+            console.log('não implementado')
+            break
+    }
+
+    return result
+}
+
+console.log(calculate(4, '-', 8))
