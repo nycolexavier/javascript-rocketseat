d33f0b2762d5501a8e71362c2889369cefa072aa
Nycole <nycole.xavierr@gmail.com>
2021-05-03 12:35:08 -0300

    Throw e Trycatch

---
+++ expressoeseoperadores/throwetrycatch.js
@@ -0,0 +1,18 @@
+// throw (disparar)
+
+function sayMyName(name = '') {
+    if (name ==='') {
+    throw new Error("Nome é necessário")
+    }
+    console.log('depois do erro')
+}
+
+
+// try...catch
+
+try{
+    sayMyName()
+
+} catch(e) {
+    console.log(e)
+}
