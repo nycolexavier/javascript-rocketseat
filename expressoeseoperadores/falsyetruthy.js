5eb176f17ab98a014543591c4c3f85c9b0e17884
Nycole <nycole.xavierr@gmail.com>
2021-05-03 10:22:50 -0300

    False e Truthy

---
+++ expressoeseoperadores/falsyetruthy.js
@@ -0,0 +1,43 @@
+/*
+    Type conversion (typecasting) vs Type coersion
+*/
+
+//console.log(Number('9') + 5)
+
+
+
+/*
+    FALSY
+    Quando um valor é considerado false em contexto onde um booleano é obrigatório (condicionais e loops)
+
+    false
+    0
+    -0
+    ""
+    null
+    undefined
+    NaN
+*/
+//console.log(0  ? 'verdadeiro' : 'falso')
+
+
+
+
+
+/*
+    TRUTHY
+    Quando um vlaor é considerado true em contextos onde um booleno é obrigatório (condicionais e loops)
+
+    true
+    {}
+    []
+    1
+    3.23
+    "0"
+    "false"
+    -1
+    Infinity
+    -Infinity
+*/
+
+console.log( {} ? 'verdadeiro' : 'falso')
