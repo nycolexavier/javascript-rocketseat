a5dccc3748dd7f07e0831df8754cd744a1f57b78
Nycole <nycole.xavierr@gmail.com>
2021-05-03 13:23:44 -0300

    For ind

---
+++ expressoeseoperadores/forin.js
@@ -0,0 +1,12 @@
+// for ... in
+
+let person = {
+    name: 'John',
+    age: 30,
+    weight: 88.6,
+}
+
+for (let property in person) {
+    console.log(property)
+    console.log(person[property])
+}
