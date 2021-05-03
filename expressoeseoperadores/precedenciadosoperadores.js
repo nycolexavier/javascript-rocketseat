0c2fb187178f91d67afe11bb3bda95f009187b33
Nycole <nycole.xavierr@gmail.com>
2021-05-03 10:31:48 -0300

    Precedência dos operadores

---
+++ expressoeseoperadores/precedenciadosoperadores.js
@@ -0,0 +1,15 @@
+/* Operator precedence
+    Precedência de operadores
+
+* grouping                  ()
+* negação e incremento      ! ++ --
+* multiplicação e divisão   * /
+* adição e subtração        + -
+* relacional                < <= > >=
+* igualdade                 == != === !==
+* AND                       &&
+* OR                        ||
+* condicional               ?:
+* assignment (atribuição)   = =+ -= *= %=
+*/
+console.log()
