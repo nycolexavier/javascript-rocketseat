7b5577b9d8040be827896afb66e0779d983a7770
Nycole <nycole.xavierr@gmail.com>
2021-05-03 10:09:31 -0300

    Operador condicional ternário

---
+++ expressoeseoperadores/operadorcondicionalternario.js
@@ -0,0 +1,22 @@
+// Operador Condicional (Ternário)
+
+// Dependendo da condição, nós receberemos valores diferentes
+
+// Condição entaõ valor 1 se não valor 2
+// condition ? value : value2
+
+// Exemplos
+// café da manhã top
+//let pao = true
+//let queijo = false
+
+//const niceBreakfast = pao || queijo ? 'Café top' : 'Café ruim'
+
+//console.log(niceBreakfast)
+
+// Maior de 18
+let age = 16
+
+const canDrive = age >= 18 ? 'can drive' : "can't drive"
+
+console.log(canDrive)
