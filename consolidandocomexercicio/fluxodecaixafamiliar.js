b6f85960f62e753a1abd9cf5df8aad9982fd60cf
Nycole <nycole.xavierr@gmail.com>
2021-05-03 21:24:22 -0300

    Fluxo de caixa familiar 1/2

---
+++ consolidandocomexercicio/fluxodecaixafamiliar.js
@@ -0,0 +1,9 @@
+/* ### Sistema de gastos familiar
+
+    Crie um obejto que possuirpa 2 propriedades, ambas do tipo array:
+        * receitas: []
+        * despesas: []
+
+    Agora, crie uma função que irpa calcular o total de receitas e despesas e irá mostrar uma mensagem se a família está com saldo positivo ou negativo, seguido do valor do saldo
+
+*/
